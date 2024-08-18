6eb1a36afac1afdb0d87d3475844463b1dfb83ca
James Mwantisi <jmwantisi@gmail.com>
2024-08-18 20:52:58 +0200

    Added Model: Applicant Program

---
+++ src/main/java/org/api/socialassistancefundapiv1/models/ApplicantProgram.java
@@ -0,0 +1,78 @@
+package org.api.socialassistancefundapiv1.models;
+import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "applicant_programs")
+public class ApplicantProgram {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Applicant getApplicant() {
+		return applicant;
+	}
+
+	public void setApplicant(Applicant applicant) {
+		this.applicant = applicant;
+	}
+
+	public Program getProgram() {
+		return program;
+	}
+
+	public void setProgram(Program program) {
+		this.program = program;
+	}
+
+	public LocalDate getApplicationDate() {
+		return applicationDate;
+	}
+
+	public void setApplicationDate(LocalDate applicationDate) {
+		this.applicationDate = applicationDate;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "applicant_id", columnDefinition = "INT", nullable = false)
+	@JsonIgnore
+    private Applicant applicant;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "program_id", columnDefinition = "INT", nullable = false)
+    @JsonIgnore
+    private Program program;
+
+    @Column(name = "application_date", nullable = true)
+    private LocalDate applicationDate;
+    
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
+
