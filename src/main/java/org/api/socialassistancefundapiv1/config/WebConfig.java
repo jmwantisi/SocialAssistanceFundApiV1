194f31be11a28a4c41b71ecf82d95e5a9c47ffd4
James Mwantisi <jmwantisi@gmail.com>
2024-08-18 20:42:12 +0200

    Allowed CORS

--- src/main/java/org/api/socialassistancefundapiv1/config/WebConfig.java
+++ src/main/java/org/api/socialassistancefundapiv1/config/WebConfig.java
@@ -1,4 +1,19 @@
 package org.api.socialassistancefundapiv1.config;
 
-public class WenConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
 }
+
