23da828bc7b24fe040228398d46122aae73cf6ff
James Mwantisi <jmwantisi@gmail.com>
2024-08-18 20:48:20 +0200

    Refactor

--- src/main/java/org/api/socialassistancefundapiv1/models/Village.java
+++ src/main/java/org/api/socialassistancefundapiv1/models/Village.java
@@ -1,6 +1,7 @@
-package org.api.socialassistancefundapiv1.model;
-
+package org.api.socialassistancefundapiv1.models;
+import java.time.LocalDateTime;
 import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "villages")
@@ -10,11 +11,53 @@ public class Village {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "name", nullable = false)
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public SubLocation getSubLocation() {
+		return subLocation;
+	}
+
+	public void setSubLocation(SubLocation subLocation) {
+		this.subLocation = subLocation;
+	}
+
+	@Column(name = "name", nullable = false)
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sub_location_id", nullable = false, columnDefinition = "INT")
+    @JsonIgnore
     private SubLocation subLocation;
+    
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 
 }
