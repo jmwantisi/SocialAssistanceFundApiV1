6194a751995de1f593b0d8a8f9353d5d2523a00a
James Mwantisi <jmwantisi@gmail.com>
2024-08-18 23:00:14 +0200

    Added by applicant program Id

--- src/main/java/org/api/socialassistancefundapiv1/repositories/ApplicantProgramRepository.java
+++ src/main/java/org/api/socialassistancefundapiv1/repositories/ApplicantProgramRepository.java
@@ -1,7 +1,11 @@
 package org.api.socialassistancefundapiv1.repositories;
+import java.util.List;
+
 import org.api.socialassistancefundapiv1.models.ApplicantProgram;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApplicantProgramRepository extends JpaRepository<ApplicantProgram, Integer> {}
+public interface ApplicantProgramRepository extends JpaRepository<ApplicantProgram, Integer> {
+	List<ApplicantProgram> findByApplicantId(Integer Id);
+}
--- src/main/java/org/api/socialassistancefundapiv1/repositories/TelephoneRepository.java
+++ src/main/java/org/api/socialassistancefundapiv1/repositories/TelephoneRepository.java
@@ -1,7 +1,11 @@
 package org.api.socialassistancefundapiv1.repositories;
+import java.util.List;
+
 import org.api.socialassistancefundapiv1.models.Telephone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TelephoneRepository extends JpaRepository<Telephone, Integer> {}
+public interface TelephoneRepository extends JpaRepository<Telephone, Integer> {
+	List<Telephone> findByApplicantId(Integer Id);
+}
