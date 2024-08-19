1812623ccf16e6a01069d5dcbb58f32547835207
James Mwantisi <jmwantisi@gmail.com>
2024-08-19 15:42:33 +0200

    refactor

--- src/main/java/org/api/socialassistancefundapiv1/repositories/LocationRepository.java
+++ src/main/java/org/api/socialassistancefundapiv1/repositories/LocationRepository.java
@@ -1,4 +1,5 @@
 package org.api.socialassistancefundapiv1.repositories;
+import java.util.List;
 import java.util.Optional;
 import org.api.socialassistancefundapiv1.models.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Integer> {
 	Optional<Location> findByName(String name);
+	List<Location> findBySubCountyId(int subCountyId);
 }
