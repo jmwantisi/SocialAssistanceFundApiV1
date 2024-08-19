7e4da5999f7b37587426873342c4dbb68c688329
James Mwantisi <jmwantisi@gmail.com>
2024-08-19 23:16:12 +0200

    fixed program controller

--- src/main/java/org/api/socialassistancefundapiv1/controllers/ProgramController.java
+++ src/main/java/org/api/socialassistancefundapiv1/controllers/ProgramController.java
@@ -1,5 +1,25 @@
 package org.api.socialassistancefundapiv1.controllers;
+import org.api.socialassistancefundapiv1.models.Program;
+import org.api.socialassistancefundapiv1.services.ProgramService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/programs")
 public class ProgramController {
 
+    private final ProgramService programService;
+
+    @Autowired
+    public ProgramController(ProgramService programService) {
+        this.programService = programService;
+    }
+
+    @GetMapping
+    public List<Program> getAllPrograms() {
+        return programService.list();
+    }
 }
+
